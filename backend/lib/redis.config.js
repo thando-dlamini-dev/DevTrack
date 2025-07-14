4aa6731831026e022353fd0a3e0540cde81e84e1
thando-dlamini-dev <thandodlaminidev@gmail.com>
2025-07-14 16:45:32 +0200

    update redis config

--- backend/lib/redis.config.js
+++ backend/lib/redis.config.js
@@ -5,11 +5,5 @@ const redis = new Redis({
     url: process.env.REDIS_ENDPOINT_URL,
     token: process.env.REDIS_TOKEN,
 })
-
-export const cacheResult = async (key, value) => {
-    await redis.set(key, value);
-}
-
-export const fetchCachedResult = async (value) => {
-    redis.get(value)
-}
+ 
+export default redis
